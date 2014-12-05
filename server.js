43fa59351b04c8df823b3ed3e8f50395084e4e3d
Rachel Sison <rsison87@gmail.com>
2014-12-04 19:50:35 -0800

    modified dependencies

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var chalk = require('chalk');
 
+
 var app = express(); //  setup express server
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
@@ -18,3 +19,24 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
 console.log("App listening on port 3000");
+
+//******************************************************************************************
+/*
+
+this is the documentation for how to create a message (api_key is legit):
+***************
+var api_key = 'key-e81b3d37fc5adcc1bc5c21f5267a90d5';
+var domain = 'selfinspi.red';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+var data = {
+  from: 'Excited User <rsison87@gmail.com>',
+  to: 'derek.barncard@gmail.com',
+  subject: 'Hello',
+  text: 'Testing some Mailgun awesomness! TEST2!!'
+};
+
+mailgun.messages().send(data, function (error, body) {
+  console.log(body);
+});
+*/
