06a3762de28bbe85b33399bbc069a3b217d6c3f9
Nikhil Ballaney <nballaney@gmail.com>
2014-12-05 21:29:27 -0800

    (feat) Setup environment for passport.js

--- db.js
+++ db.js
@@ -1,10 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new Schema({
   name: String,
   password: String
 });
+
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 var User = mongoose.model('User', userSchema);
 
@@ -19,6 +30,8 @@ var goalsSchema = new Schema({
     why: [String]
   }]
 })
+
+
 
 var Goals = mongoose.model('Goals', goalsSchema);
 
