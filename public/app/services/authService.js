2c45789b4ae4612ac1e4f988c6530b4648afc0f0
Derek Barncard <derek@barncard.com>
2014-12-06 17:44:23 -0800

    signup frontend route and page created

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -1,16 +1,29 @@
 angular.module('app.authFact', [])
 .factory('authFactory', function($q, $http){
-	var login = function(username, password){
-		console.log("authFactory getting username: " + username + ", password: " + password);
+	var login = function(email, password){
+		console.log("authFactory getting email: " + email + ", password: " + password);
 		return $http({
 			method: 'POST',
 			url: '/login',
-			data: {username: username, password: password}
+			data: {email: email, password: password}
 		}).then(function(res){
 			console.log(res);
 		});
 	}
+
+	var signup = function(email, name, password){
+		console.log("authFactory signup getting email: " + email + ", name: " + name + ", password: " + password);
+		return $http({
+			method: 'POST',
+			url: '/signup',
+			data: {email: email, name: name, password: password}
+		}).then(function(res){
+			console.log(res);
+		});
+	}
+
 	return {
-		login: login
+		login: login,
+		signup: signup
 	}
 });
